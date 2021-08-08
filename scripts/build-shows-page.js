b6bca2c20094ec988193daef2d855b6818c649b4
Violetta Petrova <vwondercame@gmail.com>
2021-08-08 00:11:11 -0400

    update for loop in JS, separated generateShowlist and generateListContent

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -56,76 +56,77 @@ let cardInfo = [
 
 const showsList = document.querySelector('.shows-list');
 
-// const generateCardContent = () => {
- 
-// }
-
-const generateCardList = () => {
-
-// cardInfo.forEach((showsData)=> {
-//   console.log(showsData); // how do I write this as a forEach loop???
-
-  for(let i = 0; i < cardInfo.length; i++) {
-    const showsData = cardInfo[i];
-    console.log(showsData);
-  
-    //create article class="shows-card" 
-    const showsCard = document.createElement('article');
-    showsCard.classList.add('shows-card');
-    showsList.appendChild(showsCard);
-  
-    // create div class="shows-card__description
-    const showsCardDescription = document.createElement('div');
-    showsCardDescription.classList.add('shows-card__description');
-    showsCard.appendChild(showsCardDescription);
-  
-    //create <div class="shows-card__content shows-card__content--date">
-    const showsCardContent = document.createElement('div');
-    showsCardContent.classList.add('shows-card__content', 'shows-card__content--date');
-    showsCardDescription.appendChild(showsCardContent);
-  
-    // create <h4 class="shows-card__sub-heading">DATES</h4>
-    const showsCardSubHeading = document.createElement('h4');
-    showsCardSubHeading.classList.add('shows-card__sub-heading');
-    showsCardSubHeading.innerText = 'DATES';
-    showsCardContent.appendChild(showsCardSubHeading);
-  
-    // create <p class="shows-card__detail">date detail</p>
-    const showsCardDetail = document.createElement('p');
-    showsCardDetail.classList.add('shows-card__detail');
-    showsCardDetail.innerText = showsData.dates;
-    showsCardContent.appendChild(showsCardDetail);
-  
-     // create <h4 class="shows-card__sub-heading">VENUES</h4>
-     const showsCardSubHeading2 = document.createElement('h4');
-     showsCardSubHeading2.classList.add('shows-card__sub-heading');
-     showsCardSubHeading2.innerText = 'VENUES';
-     showsCardContent.appendChild(showsCardSubHeading2);
-  
-       // create <p class="shows-card__detail">venue detail</p>
-      const showsCardDetail2 = document.createElement('p');
-      showsCardDetail2.classList.add('shows-card__detail');
-      showsCardDetail2.innerText = showsData.venue;
-      showsCardContent.appendChild(showsCardDetail2);
-  
+const generateCardContent = (showsData) => {
+      //create article class="shows-card" 
+      const showsCard = document.createElement('article');
+      showsCard.classList.add('shows-card');
+      showsList.appendChild(showsCard);
+    
+      // create div class="shows-card__description
+      const showsCardDescription = document.createElement('div');
+      showsCardDescription.classList.add('shows-card__description');
+      showsCard.appendChild(showsCardDescription);
+    
+      //create <div class="shows-card__content shows-card__content--date">
+      const showsCardContent = document.createElement('div');
+      showsCardContent.classList.add('shows-card__content', 'shows-card__content--date');
+      showsCardDescription.appendChild(showsCardContent);
+    
+      // create <h4 class="shows-card__sub-heading">DATES</h4>
+      const showsCardSubHeading = document.createElement('h4');
+      showsCardSubHeading.classList.add('shows-card__sub-heading');
+      showsCardSubHeading.innerText = 'DATES';
+      showsCardContent.appendChild(showsCardSubHeading);
+    
+      // create <p class="shows-card__detail">date detail</p>
+      const showsCardDetail = document.createElement('p');
+      showsCardDetail.classList.add('shows-card__detail');
+      showsCardDetail.innerText = showsData.dates;
+      showsCardContent.appendChild(showsCardDetail);
+    
+      // create <h4 class="shows-card__sub-heading">VENUES</h4>
+       const showsCardSubHeading2 = document.createElement('h4');
+       showsCardSubHeading2.classList.add('shows-card__sub-heading');
+       showsCardSubHeading2.innerText = 'VENUES';
+       showsCardContent.appendChild(showsCardSubHeading2);
+    
+      // create <p class="shows-card__detail">venue detail</p>
+        const showsCardDetail2 = document.createElement('p');
+        showsCardDetail2.classList.add('shows-card__detail');
+        showsCardDetail2.innerText = showsData.venue;
+        showsCardContent.appendChild(showsCardDetail2);
+    
       // create <h4 class="shows-card__sub-heading">LOCATION</h4>
-      const showsCardSubHeading3 = document.createElement('h4');
-      showsCardSubHeading3.classList.add('shows-card__sub-heading');
-      showsCardSubHeading3.innerText = 'LOCATION';
-      showsCardContent.appendChild(showsCardSubHeading3);
-  
+        const showsCardSubHeading3 = document.createElement('h4');
+        showsCardSubHeading3.classList.add('shows-card__sub-heading');
+        showsCardSubHeading3.innerText = 'LOCATION';
+        showsCardContent.appendChild(showsCardSubHeading3);
+    
       // create <p class="shows-card__detail">location</p>
-      const showsCardDetail3 = document.createElement('p');
-      showsCardDetail3.classList.add('shows-card__detail');
-      showsCardDetail3.innerText = showsData.location;
-      showsCardContent.appendChild(showsCardDetail3);
-  
+        const showsCardDetail3 = document.createElement('p');
+        showsCardDetail3.classList.add('shows-card__detail');
+        showsCardDetail3.innerText = showsData.location;
+        showsCardContent.appendChild(showsCardDetail3);
+
+        return showsCard;
+}
+
+const generateCardList = (cardInfo) => {
+  // cardInfo.forEach((showCard) => {
+  //   const showsData = cardInfo[i];
+  //   const showsCard = generateCardContent(showsData);
+  //   showsList.appendChild(showsCard);
+  // });
+
+    for(let i = 0; i < cardInfo.length; i++) {   //how do I write this as a forEach loop???
+    const showsData = cardInfo[i];
+    const showsCard = generateCardContent(showsData);
     showsList.appendChild(showsCard);
   }
 }
 
 // Start here
-generateCardList();
+generateCardList(cardInfo);
 
  
 
