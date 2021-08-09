6031517f25bdef2aa89028f3df7270c357cfdb35
Violetta Petrova <vwondercame@gmail.com>
2021-08-08 21:15:14 -0400

    change for loop to four each to populate comment section with array info

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -79,31 +79,64 @@ let commentInfo = [
 
 const commentSection = document.querySelector('.comment-section__list-container');
 
- const commentList = document.createElement("ul");
+//create <ul class="comment-section__list">
+    const commentList = document.createElement("ul");
     commentList.classList.add('comment-section__list');
     commentSection.appendChild(commentList);
-    console.log(commentList);
+
 //a funciton for creating comment content 
 const generateCommentContent = (commentData) => {
-    //create <ul class="comment-section__list">
-   
 
     // create <li class="comment-section__item">
     const commentItem = document.createElement("li");
-    console.log(commentItem);
     commentItem.classList.add('comment-section__item');
     commentList.appendChild(commentItem);
 
-    return commentList;
+    //create <div class="comment__avi-container">
+    const commentAviContainer = document.createElement("div");
+    commentAviContainer.classList.add('comment__avi-container');
+    commentItem.appendChild(commentAviContainer);
 
+    //create <div class="comment-section__comment"> inside commentItem
+    const commentContainer = document.createElement("div");
+    commentContainer.classList.add('comment-section__comment');
+    commentItem.appendChild(commentContainer);
+
+    //create <div class="comment-section__comment--date-time"> inside commentContainer
+    const dateTimeContainer = document.createElement("div");
+    dateTimeContainer.classList.add('comment-section__comment--date-time');
+    commentContainer.appendChild(dateTimeContainer);
+
+    //create <h3 class="comment-section__name"> Connor Walton</h3> inside dateTimeContainer
+    const commentName = document.createElement("h3");
+    commentName.classList.add('comment-section__name');
+    commentName.innerText = commentData.name;
+    dateTimeContainer.appendChild(commentName);
+    console.log(commentInfo.name);
+
+    // create <p class="comment-section__date">01/09/2021</p> inside dateTimeContainer
+    const commentDate = document.createElement("p");
+    commentDate.classList.add('comment-section__date');
+    commentDate.innerText = commentData.date;
+    dateTimeContainer.appendChild(commentDate);
+
+    //create <p class="comment-section__comment-text"> inside commentContainer
+    const commentText = document.createElement("p");
+    commentText.classList.add('comment-section__name');
+    commentText.innerText = commentData.txt;
+    commentContainer.appendChild(commentText);
+
+
+    return commentList;
 }
 
-const generateCommentList = (commentInfo) => {
-    for(let i = 0; i < commentInfo.length; i++) {   //how do I write this as a forEach loop???
-    const commentData = commentInfo[i];
-    const commentList = generateCommentContent(commentData);
-    commentSection.appendChild(commentList);
-    }
+
+const generateCommentList = (comments) => {
+    comments.forEach((item)=> {
+        const commentData = item;
+        const commentList = generateCommentContent(commentData);
+        commentSection.appendChild(commentList);
+    })
 }
 
 //start here
